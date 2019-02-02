307fe33fa759c697bcb6a36b01b76fe40d23c087
Midataur <maxpetschack2@gmail.com>
2019-02-02 11:40:48 +1100

    Version 3.1
    Clarified some things in the prompts. Added short colour names for faster typing.

--- defuser.py
+++ defuser.py
@@ -7,8 +7,8 @@ print 'KTANE Defuser by Max Petschack'
 batteries = int(raw_input('How many batteries on the bomb?: '))
 serial = int(raw_input("What's the last digit of the serial number?: "))
 vowel = True if raw_input('Is there a vowel in the serial number (y/n)?: ') == 'y' else False
-car = True if raw_input('Is there a CAR indicator on the bomb (y/n)?: ') == 'y' else False
-freak = True if raw_input('Is there a FRK indicator on the bomb (y/n)?: ') == 'y' else False
+car = True if raw_input('Is there a lit CAR indicator on the bomb (y/n)?: ') == 'y' else False
+freak = True if raw_input('Is there a lit FRK indicator on the bomb (y/n)?: ') == 'y' else False
 parallel = True if raw_input('Is there a parallel port on the bomb (y/n)?: ') == 'y' else False
 strikes = 0
 
@@ -26,7 +26,8 @@ help_text = '''Available commands:
         done, failed, add strike, help
     Module Commands:
         wires, button, symbols
-Type 'help' followed by a module command to see how that command works'''
+Type 'help' followed by a module command to see how that command works
+Type 'help short' for a list of short colour names (for faster typing)'''
 im = Image.open('ktanesymbols.png')
 
 print 'Defuser ready'
@@ -56,6 +57,8 @@ while True:
         if len(command) == 1:
             print help_text
         #specific help
+        elif command[1] == 'short':
+            print 'Colour short names: Blue (bu), Red (r), White (w), Green (g), Yellow (y), Black (bk)'
         elif command[1] == 'wires':
             print 'Usage "wires colour1 colour2 etc."'
         elif command[1] == 'button':
@@ -64,6 +67,8 @@ while True:
             im.show()
             print 'Match each symbol to its number using the image provided'
             print 'Usage "symbols number1 number2 number 3 number 4"'
+        else:
+            print 'That help page could not be found'
     #Figure out what module is being defused
     elif command[0] == 'wires':
         print simple_wires(command[1:],bomb_vars)
--- modules.py
+++ modules.py
@@ -3,43 +3,43 @@ def simple_wires(wires,bomb_vars): #simple wires (p5 on manual)
     serial = bomb_vars['serial']
     #3 wires
     if len(wires) == 3:
-        if 'red' not in wires:
+        if 'red' not in wires and 'r' not in wires:
             return 'Cut the second wire'
-        elif wires[2] == 'white':
+        elif wires[2] == 'white' or wires[2] == 'w':
             return 'Cut the last wire'
-        elif wires.count('blue') > 1:
+        elif wires.count('blue')+wires.count('bu') > 1:
             return 'Cut the last blue wire'
         else:
             return 'Cut the last wire'
     #4 wires
     elif len(wires) == 4:
-        if serial % 2 != 0 and wires.count('red') > 1:
+        if serial % 2 != 0 and wires.count('red')+wires.count('r') > 1:
             return 'Cut the last red wire'
-        elif wires[3] == 'yellow' and wires.count('red') == 0:
+        elif (wires[3] == 'yellow' or wires[3] == 'y') and wires.count('red')+wires.count('r') == 0:
             return 'Cut the first wire'
-        elif wires.count('blue') == 1:
+        elif wires.count('blue')+wires.count('bu') == 1:
             return 'Cut the first wire'
-        elif wires.count('yellow') > 1:
+        elif wires.count('yellow')+wires.count('y') > 1:
             return 'Cut the last wire'
         else:
             return 'Cut the second wire'
     #5 wires
     elif len(wires) == 5:
-        if wires[4] == 'black' and serial % 2 != 1:
+        if (wires[4] == 'black' or wires[4] == 'bk') and serial % 2 != 1:
             return 'Cut the fourth wire'
-        elif wires.count('red') == 1 and wires.count('yellow') > 1:
+        elif wires.count('red')+wires.count('r') == 1 and wires.count('yellow')+wires.count('y') > 1:
             return 'Cut the first wire'
-        elif wires.count('black') == 0:
+        elif wires.count('black')+wires.count('bk') == 0:
             return 'Cut the second wire'
         else:
             return 'Cut the first wire'
     #6 wires
     elif len(wires) == 6:
-        if wires.count('yellow') == 0 and serial % 2 != 0:
+        if wires.count('yellow')+wires.count('y') == 0 and serial % 2 != 0:
             return 'Cut the third wire'
-        elif wires.count('yellow') == 1 and wires.count('white') > 1:
+        elif wires.count('yellow')+wires.count('y') == 1 and wires.count('white')+wires.count('w') > 1:
             return 'Cut the fourth wire'
-        elif wires.count('red') == 0:
+        elif wires.count('red')+wires.count('r') == 0:
             return 'Cut the last wire'
         else:
             return 'Cut the fourth wire'
@@ -50,17 +50,17 @@ def button(button_vars,bomb_vars): #button (p6 manual)
     text = button_vars[1]
     colour = button_vars[0]
     #usage "button colour text"
-    if colour == 'blue' and text == 'abort':
+    if (colour == 'blue' or colour == 'bu') and text == 'abort':
         return button_hold()
     elif bomb_vars['batteries'] > 1 and text == 'detonate':
         return 'Press and immediately release the button'
-    elif colour == 'white' and bomb_vars['car']:
+    elif (colour == 'white' or colour == 'w') and bomb_vars['car']:
         return button_hold()
     elif bomb_vars['batteries'] > 2 and bomb_vars['freak']:
         return 'Press and immediately release the button'
-    elif colour == 'yellow':
+    elif colour == 'yellow' or colour == 'y':
         return button_hold()
-    elif colour == 'red' and text == 'hold':
+    elif (colour == 'red' or colour == 'r') and text == 'hold':
         return 'Press and immediately release the button'
     else:
         return button_hold()
@@ -70,11 +70,11 @@ def button_hold(): #for when a button needs to be held down
     colour = raw_input('What colour is the lit strip (back to return to prompt)?: ')
     if colour == 'back':
         return 'Returning to prompt'
-    elif colour == 'blue':
+    elif colour == 'blue' or colour == 'bu':
         return 'Release button when the countdown timer has a 4 in any position'
-    elif colour == 'white':
+    elif colour == 'white' or colour == 'w':
         return 'Release button when the countdown timer has a 1 in any position'
-    elif colour == 'yellow':
+    elif colour == 'yellow' or colour == 'y':
         return 'Release button when the countdown timer has a 5 in any position'
     else:
         return 'Release button when the countdown timer has a 1 in any position'
